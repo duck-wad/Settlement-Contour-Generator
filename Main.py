764506c0f9d0a55064de3a0e3636c11ab5fae022
Li <nicholas.li@hatch.com>
2025-01-29 17:12:40 -0500

    updates

--- Main.py
+++ Main.py
@@ -8,20 +8,32 @@ MeshTrans = 1         # Mesh length transverse to tunnel axis
 # Hard-coded variables
 R = 4                   # Tunnel radius (m) 
 z0 = 7.5                # Tunnel axis depth (m)
+# For now, i is a hardcoded term but it can be calculated based on the tunnel depth
 i = 1                # Settlement trough width parameter (m)
 wmax = 0.00786          # Maximum vertical settlement (m) 
+Vs = 0.025*math.pi*R*R
 
 # Function to compute the x,y,z ground movements due to tunnel excavation
 # Assumption that the tunnel face has advanced significantly far from the start of the tunnel (xf > 3z0) so the transverse profile behind the face develops fully
 def GroundMovement():
 
-    # For now, i is a hardcoded term but it can be calculated based on the tunnel depth
-
     ## Generate Mesh
-    
     Mesh = DefineMesh(i)
-    print(Mesh)
-    
+
+    # Initialize output variables. u is x, v is y, w is z displacement
+    u = []
+    v = []
+    w = []
+
+    for r in range(len(Mesh)):
+        G = NormalDistribution(Mesh[r][0] / i)
+
+        w_temp = Vs / (math.sqrt(2*math.pi*i)) * math.exp(-1*math.pow(Mesh[r][1], 2))
+        
+
+
+
+
 
 # Take the i parameter as input and discretize a rectangular mesh around the origin
 # Returns an array of tuples, each tuple is a (x,y) surface coordinate
@@ -68,5 +80,9 @@ def DefineMesh(i):
         yPos += MeshTrans
 
     return Mesh
+
+def NormalDistribution(x):
+    # Define the G function
+    return 1
 
 GroundMovement()
